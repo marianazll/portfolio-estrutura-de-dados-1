17444c663efb44e4f735485396ab537bdf67cb61
marianazll <marianamelo@alunos.utfpr.edu.br>
2026-06-27 11:29:56 -0300

    Adiciona arquivo

---
+++ 01_variaveis_operadores.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 20;
+    float altura = 1.75;
+    char letra = 'A';
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Letra: %c\n", letra);
+
+    return 0;
+}
